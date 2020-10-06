d863555337b59fda529c12b696f4b50f31c0dcf8
HectorGarciaPY <hector.garcia@iesjoandaustria.org>
2020-10-06 11:29:00 +0000

    probar el if3

---
+++ if3.py
@@ -0,0 +1,11 @@
+print("Pon un numero:")
+x=int(input())
+print("Pon otro:")
+y=int(input())
+print("Uno mas porfavor:")
+z=int(input())
+sort()
+
+>>> orden_creciente.sort() 
+>>> print orden_creciente 
+['x', 'y', 'z']
