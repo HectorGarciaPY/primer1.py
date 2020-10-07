e745794ee24b0b93cbd58241ee8844c83dfd35da
HectorGarciaPY <hector.garcia@iesjoandaustria.org>
2020-10-07 10:16:02 +0000

    if4 acabado

---
+++ if4.py
@@ -0,0 +1,11 @@
+print("Pon un numero:")
+x=int(input())
+print("Pon otro:")
+y=int(input())
+z=y*x
+if z==0:
+  print("El producto es 0.")
+if z<0:
+  print("El producto es menor que 0.")
+if z>0:
+  print("El producto es mayor que 0.")
