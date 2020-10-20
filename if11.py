dbf5ac2747f1738a52b7008c56b62a77236d1c65
HectorGarciaPY <hector.garcia@iesjoandaustria.org>
2020-10-20 11:33:42 +0000

    if 11 revisar y probar

--- if11.py
+++ if11.py
@@ -32,16 +32,46 @@ if a >=0 :
     print("Es incorrecta...")
 else:
   print("Es incorrecta...")
-if seg+1== 60:
-  print("Si le sumo un segundo a la hora que me has dicho seran las:", h ,":", mi+1,":", 0,".")
-  if mi+1==60:
-   print("Si le sumo un segundo a la hora que me has dicho seran las:", h+1 ,":", 0,":", seg,".")
-   if h+1==24:
-    print("Si le sumo un segundo a la hora que me has dicho seran las:", 0 ,":", mi,":", seg,".")
-
-if mi+1==60:
-  h+1
-if h >=0 and h<=24:
-  if mi>=0 and mi<=59:
-    if seg>=0 and seg<=59:
-      print("Si le sumo un segundo a la hora que me has dicho seran las:", h ,":", mi,":", x,".")
+if seg<= 59 and seg>=0:
+  if mi<=59 and mi>=0:
+    if h<=23 and h>=0:
+      if seg== 59:
+        if mi==59:
+          if h==23:
+            if a>=0:
+              if m == 1 or m==3 or m == 5 or m==7 or m == 8 or m==10:
+                if d==31:
+                  print("Si le sumo un segundo a la hora que me has dicho sera:",1 ,"/",m+1 ,"/", a,"Hora:", 0,":",0,":",0 )
+                else:
+                  print("Si le sumo un segundo a la hora que me has dicho sera:",d+1 ,"/",m ,"/", a,"Hora:", 0,":",0,":",0 )
+              elif m== 4 or m == 6 or m==9 or m == 11:
+                if d==30:
+                  print("Si le sumo un segundo a la hora que me has dicho sera:",1 ,"/",m+1 ,"/", a,"Hora:", 0,":",0,":",0 )
+                else:
+                  print("Si le sumo un segundo a la hora que me has dicho sera:",d+1 ,"/",m ,"/", a,"Hora:", 0,":",0,":",0 )
+              elif m== 2:
+                if calendar.isleap(a):
+                  if d==29:
+                    print("Si le sumo un segundo a la hora que me has dicho sera:",1 ,"/",m+1 ,"/", a,"Hora:", 0,":",0,":",0 )
+                  else:
+                    print("Si le sumo un segundo a la hora que me has dicho sera:",d+1 ,"/",m ,"/", a,"Hora:", 0,":",0,":",0 )
+                elif not calendar.isleap(a):
+                  if d==28:
+                    print("Si le sumo un segundo a la hora que me has dicho sera:",1 ,"/",m+1 ,"/", a,"Hora:", 0,":",0,":",0 )
+              elif m==12:
+                if d==31:
+                  print("Si le sumo un segundo a la hora que me has dicho sera:",1 ,"/",1 ,"/", a+1,"Hora:", 0,":",0,":",0 )
+                else:
+                  print("Si le sumo un segundo a la hora que me has dicho sera:",d+1 ,"/",m ,"/", a,"Hora:", 0,":",0,":",0 )
+          else:
+            print("Si le sumo un segundo a la hora que me has dicho sera:",d ,"/",m ,"/", a,"Hora:", h+1,":",0,":",0 )
+        else:
+          print("Si le sumo un segundo a la hora que me has dicho sera:",d ,"/",m ,"/", a,"Hora:", h,":",mi+1,":",0 )             
+      else:
+          print("Si le sumo un segundo a la hora que me has dicho sera:",d ,"/",m ,"/", a,"Hora:", h,":",mi,":",seg+1 )
+    else:
+      print("La hora introducida no es correcta.")
+  else:
+    print("La hora introducida no es correcta.")    
+else:
+  print("La hora introducida no es correcta.")
